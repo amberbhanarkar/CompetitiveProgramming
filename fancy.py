5358ee21ea1fa9b396ff42bb89b72bbd9945ec53
Amber Bhanarkar <amberbhanarkar@gmail.com>
2019-06-07 17:08:59 +0530

    Hackerrank

---
+++ fancy.py
@@ -0,0 +1,19 @@
+T = int(input())
+a = []
+for i in range(0,T):
+    S = str(input())
+    a = S.split()
+    #print(a)
+    if "not" in a :
+        print("Real Fancy")
+    elif "n't" in S:
+        print("Real Fancy")
+    else:
+        print("regularly fancy")
+
+
+
+
+
+
+
