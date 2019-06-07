be6de461d5273cea463781f82ab7e9ea3f51cb2e
Amber Bhanarkar <amberbhanarkar@gmail.com>
2019-06-07 17:51:51 +0530

    Hackerank

--- left_rotation.c
+++ left_rotation.c
@@ -1,50 +1,35 @@
 #include<stdio.h>
 
-void left_rotate(int arr[], int n, int d)
+void left_rotate(unsigned long int arr[], unsigned long int n, unsigned long int d)
 {
-	int a2[n];
-	for(int i=0;i<n;i++)
-	{
-		a2[i]=0;
-	}
-	printf("\n");
-	for(int i=0;i<n;i++)
-	{
-		printf("%d", a2[i]);
-	}
-	for(int i=d;i<n;i++)
-	{
-		a2[i-d]=arr[i];
-	}
-	printf("\n");
-	for(int i=0;i<n;i++)
-	{
-		printf("%d", a2[i]);
-	}
+	unsigned long int a2[n], cnt=0;
 	for(int i=0;i<d;i++)
 	{
-		a2[i+d]=arr[i];
+		for(unsigned long int i=1;i<n;i++)
+		{
+			a2[i-1] = arr[i];
+		}
+		a2[n-1] = arr[0];
+		for(unsigned long int i=0;i<n;i++)
+		{
+			arr[i]=a2[i];
+		}
 	}
-	printf("\n");
-	for(int i=0;i<n;i++)
+	for(unsigned long int i=0;i<n;i++)
 	{
-		printf("%d", a2[i]);
+		printf("%lu ", a2[i]);
 	}
 }
 
 int main()
 {
-	int n,d,num,i;
-	scanf("%d %d", &n,&d);
-	int a[n];
+	unsigned long int n,d,num,i;
+	scanf("%lu %lu", &n,&d);
+	unsigned long int a[n];
 	for(i=0;i<n;i++)
 	{
-		scanf("%d", &num);
+		scanf("%lu", &num);
 		a[i]=num;
-	}
-	for(int i=0;i<n;i++)
-	{
-		printf("%d", a[i]);
 	}
 	left_rotate(a,n,d);
 	return 0;
