5078abcf53cf5cf6eb3462f36e8d389170b7015b
Amber Bhanarkar <41240819+amberbhanarkar@users.noreply.github.com>
2020-08-21 13:17:09 +0530

    Add files via upload

---
+++ DORA.py
@@ -0,0 +1,11 @@
+T = int(input())
+mat= []
+for i in range(0,T):
+    N, M = map(int,input().split())
+    for j in range(0,N):
+        A = list(map(int,input().split()))
+        mat.append(A)
+    #print(mat)
+    for j in range(0,N):
+        
+        
