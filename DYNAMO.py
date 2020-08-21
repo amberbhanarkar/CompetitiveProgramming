e1efdba4e3d1f417de538a04e19d1da6687be161
Amber Bhanarkar <41240819+amberbhanarkar@users.noreply.github.com>
2020-08-21 13:17:11 +0530

    Add files via upload

---
+++ DYNAMO.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    N = int(input())
+    A = int(input())
+    S = A + 2*((10**N)-1)
+    print(S, flush=True)
+    B = int(input())
+    C = (10**N)-B
+    print(C, flush=True)
+    D = int(input())
+    E = (10**N)-D
+    print(E, flush=True)
+    a = int(input())
+    if a==-1:
+        #int(input())
+        exit()
